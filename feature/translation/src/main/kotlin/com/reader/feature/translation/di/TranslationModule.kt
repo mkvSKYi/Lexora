853e6c7b5f5060e23e6cae32f6a89f107ee455af
Ihor Makovskyi <ihor.makovskyi@spreadgroup.com>
2026-06-25 14:20:10 +0200

    feat: add TtsSpeaker for word pronunciation

--- feature/translation/src/main/kotlin/com/reader/feature/translation/di/TranslationModule.kt
+++ feature/translation/src/main/kotlin/com/reader/feature/translation/di/TranslationModule.kt
@@ -2,6 +2,8 @@ package com.reader.feature.translation.di
 
 import com.reader.feature.translation.MlKitTranslationEngine
 import com.reader.feature.translation.TranslationEngine
+import com.reader.feature.translation.tts.AndroidTtsSpeaker
+import com.reader.feature.translation.tts.TtsSpeaker
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -14,4 +16,8 @@ abstract class TranslationModule {
     @Binds
     @Singleton
     abstract fun bindTranslationEngine(impl: MlKitTranslationEngine): TranslationEngine
+
+    @Binds
+    @Singleton
+    abstract fun bindTtsSpeaker(impl: AndroidTtsSpeaker): TtsSpeaker
 }
